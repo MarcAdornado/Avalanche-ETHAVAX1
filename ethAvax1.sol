a1df20918e1fdfab3412d40fe9864c3a513ad6ec
MarcAdornado <202110010@fit.edu.ph>
2024-11-30 16:55:14 +0800

    Update ethAvax1.sol

--- ethAvax1.sol
+++ ethAvax1.sol
@@ -3,14 +3,14 @@ pragma solidity ^0.8.27;
 
 contract NumberValidator {
     
-    function checkMultiple(int _num) public pure returns (string memory) {
-        require(_num > 0, "Input must be positive number");
+    function checkMultiple(int num) public pure returns (string memory) {
+        require( num > 0, "Input must be positive number");
         
-        if (_num % 5 != 0) {
+        if (num % 5 != 0) {
             revert("The number is not a multiple of 5");
         }
         
-        assert(_num % 5 == 0);
+        assert( num % 5 == 0);
         
         return "The number is a multiple of 5";
     }
